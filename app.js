13f2117848962d834341435b3f8ed5301b35b824
Sourav <srvsarkar2023@gmail.com>
2024-09-19 18:28:04 +0530

    ready

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const mongoose = require("mongoose");
 const auth = require("./router/auth");
 const list = require("./router/list");
 const cors = require("cors");
-const path = require("path");
+require("dotenv").config
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+require('dotenv').config()
 
-// MongoDB connection
-const dbUrl = 'mongodb+srv://srvsarkar2018:fJZwDiIIJkqpjGQm@cluster0.1n8al.mongodb.net/';
+const port = process.env.PORT;
+
 async function main() {
-  await mongoose.connect(dbUrl);
+  await mongoose.connect(process.env.MONGO_URL);
 }
 main()
   .then(() => {
@@ -20,25 +21,14 @@ main()
   })
   .catch((err) => console.log(err));
 
-// Serve static files
-app.use(express.static(path.resolve(__dirname, "frontend", "build")));
 
 // Routes
 app.use("/auth", auth);
 app.use("/list", list);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-});
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
 
-// Start the server
-const PORT = process.env.PORT || 1000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
